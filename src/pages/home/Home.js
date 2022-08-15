afe24d1cb3364207ff1c3c60f161858003df3c5a
Thanh Le <thanhlevan0203@gmail.com>
2022-08-14 17:56:33 -0700

    Created Post & Sidebar components

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Header from '../../header/Header'
+import Posts from '../../posts/Posts'
+import Sidebar from '../../sidebar/Sidebar'
 import "./home.css"
 
 const Home = () => {
   return (
-    <div className='home'>
+    <>
         <Header />
-        HomePage
-    </div>
+        <div className='home'>
+            <Posts />
+            <Sidebar />
+        </div>
+    </>
+    
   )
 }
 
