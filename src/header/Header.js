1a801447b84da744591739f266c5533c1187cc24
Thanh Le <thanhlevan0203@gmail.com>
2022-08-14 17:48:27 -0700

    Styling Header Page

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,7 +4,15 @@ import "./header.css"
 const Header = () => {
   return (
     <div className='header'>
-        Header
+        <div className='headerTitles'>
+            <span className='headerTitleSm'>React & Node</span>
+            <span className='headerTitleLg'>Blog</span>
+        </div>
+        <img
+            className='headerImg'
+            src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nature-quotes-landscape-1648265648.jpg?crop=1.00xw:0.752xh;0,0.0817xh&resize=640:*'
+            alt=''
+        />
     </div>
   )
 }
