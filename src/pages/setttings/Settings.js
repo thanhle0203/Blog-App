b94c52e6faacc7fdba233ffbe6b6d3131cf6b1a4
Thanh Le <thanhlevan0203@gmail.com>
2022-08-16 15:52:27 -0700

    Styling Setting page

--- src/pages/setttings/Settings.js
+++ src/pages/setttings/Settings.js
@@ -7,7 +7,7 @@ export const Settings = () => {
     <div className='settings'>
       <div className='settingsWrapper'>
         <div className='settingsTitle'>
-          <span className='setttingsUpdateTitle'>Update Your Account</span>
+          <span className='settingsUpdateTitle'>Update Your Account</span>
           <span className='settingsDeleteTitle' >Delete Account</span>
         </div>
         <form className='settingsForm'>
@@ -28,7 +28,7 @@ export const Settings = () => {
           <input type='email' placeholder="thanhlevan0203@gmail.com" />
           <label>Password</label>
           <input type='password' />
-          <button className='settingsSubmit' type='submit'>Update</button>
+          <button className='settingsButton' type='submit'>Update</button>
         </form>
       </div>
       <Sidebar />
