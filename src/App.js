c09c5f7e51fb4cff14c9d55b4bdac96be75cfbf0
Thanh Le <thanhlevan0203@gmail.com>
2022-08-16 14:24:36 -0700

    Created Write component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar"
 import SinglePage from "./pages/SinglePage/SinglePage";
+import { Write } from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <TopBar />
-      <SinglePage />
+      <Write />
     </>
     
   );
