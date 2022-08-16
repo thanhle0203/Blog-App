573c3e9367e524ad43b894d18e9f666b12594870
Thanh Le <thanhlevan0203@gmail.com>
2022-08-16 14:52:08 -0700

    Created a new post component

--- src/pages/write/Write.js
+++ src/pages/write/Write.js
@@ -7,7 +7,7 @@ export const Write = () => {
         <form className='writeForm'>
             <div className='writeFormGroup'>
                 <label htmlFor='fileInput' >
-                    <i class="fa-solid fa-plus"></i>
+                    <i className="writeIcon fa-solid fa-plus"></i>
                 </label>
                 <input type="file" id="fileInput" style={{display:"none"}} />
                 <input type="text" placeholder='Title' className='writeInput' autoFocus={true} />
