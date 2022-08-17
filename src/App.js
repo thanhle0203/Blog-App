fbb6bfd4986b58889141c2f5f9266bd8eab822da
Thanh Le <thanhlevan0203@gmail.com>
2022-08-16 22:45:04 -0700

    Added Logic Link

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 
 function App() {
+  const user = true;
+
   return (
     <Router>
       <TopBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/write" element={<Write />} />
-        <Route exact path="/settings" element={<Settings />} />
+        <Route exact path="/register" element={user ? <Home/> : <Register />} />
+        <Route exact path="/login" element={user ? <Home/> : <Login />} />
+        <Route exact path="/write" element={user ? <Write/> : <Register />} />
+        <Route exact path="/settings" element={user ? <Settings/> : <Register />} />
         <Route exact path="/post/:postId" element={<SinglePage />} />
       </Routes>
      
