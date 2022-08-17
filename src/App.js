3fcf92f894c176e391d9335dfb7b6d7146bb44d8
Thanh Le <thanhlevan0203@gmail.com>
2022-08-16 23:11:30 -0700

    Added Link to page in Topbar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 
 function App() {
-  const user = true;
+  const user = false;
 
   return (
     <Router>
--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import "./topbar.css"
 
 const TopBar = () => {
+  const user = false;
   return (
     <div className='top'>
         <div className='topLeft'>
@@ -11,19 +13,36 @@ const TopBar = () => {
         </div>
         <div className='topCenter'>
             <ul className='topList'>
-                <li className='topListItem'>HOME</li>
-                <li className='topListItem'>ABOUT</li>
-                <li className='topListItem'>CONTACT</li>
-                <li className='topListItem'>WRITE</li>
-                <li className='topListItem'>LOGOUT</li>
+                <li className='topListItem'><Link className='link' to="/">HOME</Link></li>
+                <li className='topListItem'><Link className='link' to="/about">ABOUT</Link></li>
+                <li className='topListItem'><Link className='link' to="/contact">CONTACT</Link></li>
+                <li className='topListItem'><Link className='link' to="/write">WRITE</Link></li>
+                <li className='topListItem'>
+                    {user && "LOGOUT"}
+                </li>
             </ul>
         </div>
         <div className='topRight'>
-            <img 
-                className='topImg'
-                src = "https://neilpatel.com/wp-content/uploads/2018/10/blog.jpg"
-                alt=''
-            />
+            {
+                user ? (
+                    <img 
+                        className='topImg'
+                        src = "https://neilpatel.com/wp-content/uploads/2018/10/blog.jpg"
+                        alt=''
+                    />
+                ) : (
+                    <ul className='topList'>
+                        <li className='topListItem'>
+                            <Link className='link' to="/login">LOGIN</Link>
+                        </li>
+                        <li className='topListItem'>
+                            <Link className='link' to="/register">REGISTER</Link>
+                        </li>
+
+                    </ul>
+                )
+            }
+            
             <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
         </div>
     </div>
