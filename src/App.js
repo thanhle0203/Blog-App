52f48681accc53925eb7c11643330f4d1fc21f04
Thanh Le <thanhlevan0203@gmail.com>
2022-08-16 22:38:08 -0700

    Added Router path

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import { Write } from "./pages/write/Write";
 import { Settings } from "./pages/setttings/Settings";
 import { Login } from "./pages/Login/Login";
 import { Register } from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 
 function App() {
   return (
-    <>
+    <Router>
       <TopBar />
-      <Register />
-    </>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/write" element={<Write />} />
+        <Route exact path="/settings" element={<Settings />} />
+        <Route exact path="/post/:postId" element={<SinglePage />} />
+      </Routes>
+     
+    </Router>
     
   );
 }
