113fe6a0b8f417b37f5399a34e3fcab653287132
Thanh Le <thanhlevan0203@gmail.com>
2022-08-16 18:52:21 -0700

    Created Login Pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import TopBar from "./components/topbar/TopBar"
 import SinglePage from "./pages/SinglePage/SinglePage";
 import { Write } from "./pages/write/Write";
 import { Settings } from "./pages/setttings/Settings";
+import { Login } from "./pages/Login/Login";
 
 
 function App() {
   return (
     <>
       <TopBar />
-      <Settings />
+      <Login />
     </>
     
   );
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,6 +3,15 @@ import "./login.css"
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login'>
+        <form className='loginForm'>
+            <label>Email</label>
+            <input type='text' placeholder='Enter your email...' />
+            <label>Password</label>
+            <input  type='password' placeholder='Enter your password...' />
+            <button className='loginButton'>Login</button>
+        </form>
+        <button className='loginRegisterButton'>Register</button>
+    </div>
   )
 }
